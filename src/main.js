86eb6b1df4d30a654a672dae3cd97dc58555b668
Ajitha Vijayakumar <ajitha7472@gmail.com>
2024-02-01 16:34:02 +0530

    until edit books

--- src/main.js
+++ src/main.js
@@ -9,8 +9,6 @@ function Main(){
     const [author, setAuthor] = useState('');
     const [category, setCategory] = useState('');
     const [description, setDescription] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const [showModal, setShowModal] = useState(false); 
     const [editModal, setEditModal] = useState(false); 
     useEffect(() => {
@@ -61,22 +59,28 @@ function Main(){
         }
     };
     const resetFields = () => {
-        setEmail('');
-        setPassword('');
+        setbookName('');
+        setAuthor('');
+        setDescription('');
+        setCategory('');
       };
-      const handleEdit = (user) => {
-        setId(user._id);
-        setEmail(user.username);
-        setPassword(user.password);
+      const handleEdit = (book) => {
+        setId(book._id);
+        setbookName(book.bookName);
+        setAuthor(book.author);
+        setDescription(book.description);
+        setCategory(book.category);
         setEditModal(true);
       };
-      const handleEditUser = async () => {
-        const response = await axios.post('http://127.0.0.1:8000/editUser', {
+      const handleEditBook = async () => {
+        const response = await axios.post('http://127.0.0.1:8000/editBook', {
           id: id,
-          username: email,
-          password: password,
+          bookName : bookName,
+          author:author,
+          description :description,
+          category:category
         });
-        console.log('User edited successfully:', response.data);
+        console.log('Details edited successfully:', response.data);
         setEditModal(false);
         alert("Successfully edited");
         fetchTableData();
@@ -133,25 +137,35 @@ function Main(){
           <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title">Edit User</h5>
+                <h5 className="modal-title">Edit Book Details</h5>
                 <button type="button" className="close" onClick={() => setEditModal(false)}>
                   &times;
                 </button>
               </div>
               <div className="modal-body">
               
-                <form onSubmit={handleEditUser}>
+                <form onSubmit={handleEditBook}>
 
                 <div className="form-group py-4">
-                  <label htmlFor="exampleInputEmail1">Email address</label>
-                  <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={email}
-                          onChange={(e) => setEmail(e.target.value)}/>
+                  <label htmlFor="bookName">Book Title</label>
+                  <input type="text" className="form-control" id="bookName" aria-describedby="bookName" placeholder="Enter Book Title" value={bookName}
+                          onChange={(e) => setbookName(e.target.value)}/>
                 </div>
                 <div className="form-group py-4">
-                  <label htmlFor="exampleInputPassword1">Password</label>
-                  <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={password}
-                          onChange={(e) => setPassword(e.target.value)}/>
-                </div> 
+                  <label htmlFor="author">Author</label>
+                  <input type="text" className="form-control" id="author" aria-describedby="author" placeholder="Enter Author Name" value={author}
+                          onChange={(e) => setAuthor(e.target.value)}/>
+                </div>
+                <div className="form-group py-4">
+                  <label htmlFor="description">Description</label>
+                  <input type="text" className="form-control" id="description" aria-describedby="description" placeholder="Enter Description about the Book" value={description}
+                          onChange={(e) => setDescription(e.target.value)}/>
+                </div>
+                <div className="form-group py-4">
+                  <label htmlFor="category">Category</label>
+                  <input type="text" className="form-control" id="category" aria-describedby="category" placeholder="Enter the Category" value={category}
+                          onChange={(e) => setCategory(e.target.value)}/>
+                </div>
                   <button type="submit" className="btn btn-primary">Edit</button>
                 </form>
               </div>
